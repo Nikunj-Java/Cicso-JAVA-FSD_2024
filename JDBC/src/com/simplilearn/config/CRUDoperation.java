73c4e8a79fb9d4b086826738009706dbcf8a0ad2
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-08 15:17:03 +0530

    Stream and Stream with Objects,Maven,JDBC,CRUD

---
+++ JDBC/src/com/simplilearn/config/CRUDoperation.java
@@ -0,0 +1,38 @@
+package com.simplilearn.config;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class CRUDoperation {
+
+	public static void main(String[] args) {
+		Connection conn = DBConfigJDBC.getConnection();
+
+		if (conn != null) {
+			System.out.println("Connections Established");
+
+			try {
+				// 01 .Insert a Record
+				Statement stmt=conn.createStatement();
+				String query = "insert into eproduct(name,price)value('xyz-Laptop',20000)";
+				// types of Statements:
+				// 1--> Prepared Statements--> Dynamic Data
+				// 2--> Callable Statements--> Stored procedure
+				// 3---> Statements---> static Query
+				int count=stmt.executeUpdate(query);
+			System.out.println(count +" Record Updated");
+				
+				
+
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		} else {
+			System.out.println("Error in Connections");
+		}
+
+		// 01 insert record
+
+	}
+
+}
---
+++ JDBC/src/com/simplilearn/config/DBConfigJDBC.java
@@ -0,0 +1,42 @@
+package com.simplilearn.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConfigJDBC {
+	
+	static  Connection getConnection() {
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/cisco2024"; //cisco2024 is my database name
+		String username="root";
+		String password="Nikunj@123";
+		
+		
+		
+		//02. getConnectivity
+		Connection conn=null;
+		
+		try {
+			
+			//01. load the Driver
+			Class.forName(driver);
+			conn=DriverManager.getConnection(url,username,password);
+			
+			//03. check the connections
+			
+			if(conn!=null) {
+				return conn;
+			}
+			else {
+				return null;
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+
+}
