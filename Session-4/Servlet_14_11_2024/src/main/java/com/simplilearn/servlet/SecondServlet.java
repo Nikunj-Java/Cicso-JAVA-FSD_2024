fedda35efcb73bb8e15510ac43bc310f425c4d2c
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-14 11:37:26 +0530

    stored procedure, Servlet App

---
+++ Session-4/Servlet_14_11_2024/src/main/java/com/simplilearn/servlet/SecondServlet.java
@@ -0,0 +1,22 @@
+package com.simplilearn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecondServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		
+		PrintWriter out= resp.getWriter();
+		out.println("Welcome to Second Servlet");
+	}
+
+}
