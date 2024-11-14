ce580a9332270a40861a8239909be21c481d52d6
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-14 12:19:53 +0530

    stored procedure, Servlet App, Login Servlet

---
+++ Session-4/Servlet_14_11_2024/src/main/java/com/simplilearn/servlet/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.simplilearn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		
+		String name=req.getParameter("uname");
+		String pass=req.getParameter("upass");
+		
+		out.println("Welcome!.."+name);
+		out.println("Password: "+pass);
+		
+		
+	}
+	
+
+}
