36708a1461866d3adb4b43ff1144d5f9d2404695
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-15 13:06:32 +0530

    Http Session,Hidden Fields,URL Rewritting,Cookies

--- Session-5/CookieExample/src/main/java/com/simplilearn/demo/LoginSevlet.java
+++ Session-5/CookieExample/src/main/java/com/simplilearn/demo/LoginSevlet.java
@@ -1,23 +1,25 @@
-package com.cimplilearn.cookie;
+package com.simplilearn.demo;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+ 
 
 @WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+public class LoginSevlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		String user= req.getParameter("uname");
-		Cookie c=new Cookie("username", user);
+		String user= req.getParameter("user_id");
+		Cookie c=new Cookie("registered", user);
 		resp.addCookie(c);
 		resp.sendRedirect("dashboard");
 		
