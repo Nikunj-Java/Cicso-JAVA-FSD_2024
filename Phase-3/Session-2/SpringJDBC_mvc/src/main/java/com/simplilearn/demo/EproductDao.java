54de561d09e5760c197bc970e88e92fe02bce3b8
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-28 16:00:17 +0530

    Spring AOp,SPRING MVC,Spring JDBC

---
+++ Phase-3/Session-2/SpringJDBC_mvc/src/main/java/com/simplilearn/demo/EproductDao.java
@@ -0,0 +1,35 @@
+package com.simplilearn.demo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EproductDao {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public List<Eproduct> getAllProducts(){
+		return  jdbcTemplate.query("select * from eproduct", new RowMapper<Eproduct>() {
+
+			@Override
+			public Eproduct mapRow(ResultSet rs, int rowNum) throws SQLException {
+				// TODO Auto-generated method stub
+			Eproduct obj=new Eproduct();
+			obj.setId(rs.getLong(1));
+			obj.setName(rs.getString(2));
+			obj.setPrice(rs.getBigDecimal(3));
+			obj.setDate_added(rs.getTimestamp(4));
+			return obj;
+			}
+			
+		});
+	}
+
+}
