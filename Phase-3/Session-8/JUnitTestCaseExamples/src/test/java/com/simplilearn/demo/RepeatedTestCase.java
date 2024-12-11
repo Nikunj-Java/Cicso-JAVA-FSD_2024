9ef09772cab62b1f944016833ee77512d19df595
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-12-11 12:34:48 +0530

    JUnit Test Cases,Nested TestCase,Dynamic Test Case,RepeatedTestCase

---
+++ Phase-3/Session-8/JUnitTestCaseExamples/src/test/java/com/simplilearn/demo/RepeatedTestCase.java
@@ -0,0 +1,43 @@
+package com.simplilearn.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class RepeatedTestCase {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("Before All");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("After All");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("Before Each");
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("After Each");
+	}
+
+	@Test
+	@DisplayName("Main Test Case")
+	@RepeatedTest (value = 5,name = "TestCase :{displayName} the currentcount {currentRepetition} out of {totalRepetitions}")
+	void test() {
+		assertEquals(10, 6+4);
+		System.out.println("=======TEST COMPLETED=======");
+	}
+
+}
